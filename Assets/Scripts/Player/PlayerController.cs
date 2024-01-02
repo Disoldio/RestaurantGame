04d294a83b9aafad930dcf4be3d7b1afbd779c18
Oceangrad <peterportal112@gmail.com>
2024-01-02 18:41:08 +0300

    Remade PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,34 +8,64 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 12f;
     public float lookSpeed = 0.5f;
 
-    private Vector3 moveDirection = Vector3.zero;
+    /*private Vector3 moveDirection = Vector3.zero;*/
+    private Vector3 lookDirection = Vector3.zero;
     private CharacterController characterController;
+    private Quaternion currentRotation;
 
     private bool canMove = true;
+    private float currentSpeed;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        currentRotation = transform.rotation;
+        currentSpeed = walkSpeed;
     }
 
     void Update()
     {
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
+        Move();
+    }
+
+    void Move()
+    {
+        /*        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);*/
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        currentSpeed = canMove ? (isRunning ? runSpeed : walkSpeed) : 0;
 
-        characterController.Move(moveDirection * Time.deltaTime);
+        /*        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+                float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+                float movementDirectionY = moveDirection.y;
+                moveDirection = (forward * curSpeedX) + (right * curSpeedY);*/
 
-        if (canMove)
+        lookDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        /*lookDirection = (forward * Input.GetAxis("Vertical")) + (right * Input.GetAxis("Horizontal"));*/
+
+        characterController.Move(lookDirection * currentSpeed * Time.deltaTime);
+
+        bool isMoving =
+            Input.GetAxisRaw("Horizontal") != 0 ||
+            Input.GetAxisRaw("Vertical") != 0;
+        if (isMoving)
         {
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Horizontal") * lookSpeed, 0);
+            currentRotation = Quaternion.LerpUnclamped(
+                transform.localRotation,
+                Quaternion.LookRotation(lookDirection, transform.up),
+                lookSpeed
+            );
         }
+
+        transform.localRotation = currentRotation;
+
+        /*        if (canMove)
+                {
+                    transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Horizontal") * lookSpeed, 0);
+                }*/
     }
 }
