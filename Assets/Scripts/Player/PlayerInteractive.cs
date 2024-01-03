2142823a4e6ba0ebabda4e52ea1e251bbb91e058
Disoldio <klish.andr@yandex.ru>
2024-01-03 18:08:55 +0300

    add items

--- Assets/Scripts/Player/PlayerInteractive.cs
+++ Assets/Scripts/Player/PlayerInteractive.cs
@@ -27,17 +27,26 @@ public class PlayerInteractive : MonoBehaviour
         RaycastHit hit;
 
         if(Physics.Raycast(gameObject.transform.position + new Vector3(0, 1, 0), gameObject.transform.forward, out hit, distance))
-        {
-            if(hit.transform.tag == "Item")
+        {   
+            if (canPickUp) Drop();
+
+            if (hit.transform.GetComponent<Container>())
             {
-                if (canPickUp) Drop();
-                currentItem = hit.transform.gameObject;
-                currentItem.GetComponent<Rigidbody>().isKinematic = true;
-                currentItem.transform.parent = transform;
-                currentItem.transform.localPosition = new Vector3(0, 1, 1.2f);
-                currentItem.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
-                canPickUp = true;
+                Container container = hit.transform.GetComponent<Container>();
+                currentItem = container.GetItemFromContainer();
             }
+            
+            if (hit.transform.tag == "Item")
+            {
+                currentItem = hit.transform.gameObject;
+            }
+
+            currentItem.GetComponent<Rigidbody>().isKinematic = true;
+            currentItem.transform.parent = transform;
+            currentItem.transform.localPosition = new Vector3(0, 1, 1.2f);
+            currentItem.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            canPickUp = true;
+
             print(hit.transform.gameObject);
         }
     }
